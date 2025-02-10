7e4cf7da8d50303baf1a84857c1d74b67b5eff05
PieYots <yotsarawoot.c@gmail.com>
2025-02-11 04:25:53 +0700

    Update OJT to have instruction type

--- app/Models/OjtRecord.php
+++ app/Models/OjtRecord.php
@@ -14,6 +14,7 @@ class OjtRecord extends Model
     protected $fillable = [
         'employee_id',
         'type',
+        'type_of_instruction',
         'topic',
         'start_date',
         'end_date',
