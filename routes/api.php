b792d65ed98c15d372df9767a3d1508a0a5da2ea
PieYots <yotsarawoot.c@gmail.com>
2025-02-18 12:39:10 +0700

    Update api.php
    fix route

--- routes/api.php
+++ routes/api.php
@@ -81,25 +81,3 @@ Route::prefix('work-types')->group(function () {
     Route::get('/', [WorkTypeController::class, 'index']); // Get all work types
     Route::get('/{id}', [WorkTypeController::class, 'show']); // Get work type by ID
 });
-
-Route::prefix('opls')->group(function () {
-    Route::get('/', [OPLController::class, 'index']); // Get all OPLs
-    Route::get('/{id}', [OPLController::class, 'show']); // Get OPL by ID
-    Route::post('/', [OPLController::class, 'store']); // Create OPL
-    Route::delete('/{id}', [OPLController::class, 'destroy']); // Delete OPL
-});
-
-Route::prefix('improvements')->group(function () {
-    Route::get('/', [ImprovementController::class, 'index']); // Get all improvements
-    Route::get('/{id}', [ImprovementController::class, 'show']); // Get improvement by ID
-    Route::post('/', [ImprovementController::class, 'store']); // Create improvement
-    Route::delete('/{id}', [ImprovementController::class, 'destroy']); // Delete improvement
-});
-
-// Routes for Improvements
-Route::prefix('improvements')->group(function () {
-    Route::get('/', [ImprovementController::class, 'index']); // Get all improvements
-    Route::get('/{id}', [ImprovementController::class, 'show']); // Get improvement by ID
-    Route::post('/', [ImprovementController::class, 'store']); // Create a new improvement
-    Route::delete('/{id}', [ImprovementController::class, 'destroy']); // Delete an improvement
-});
